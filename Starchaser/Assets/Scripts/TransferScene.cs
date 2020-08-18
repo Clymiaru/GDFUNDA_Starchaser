ece668c6b1d57d9d84b8a52318d68e23ca08474e
Clymiaru <lscirno@gmail.com>
2020-08-18 18:25:13 +0800

    Imported recent version of Floor 3 and Floor 8 white room and added other packages.

---
+++ Starchaser/Assets/Scripts/TransferScene.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TransferScene : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision.gameObject.name);
+
+        if (SceneManager.GetActiveScene().name == "Floor3")
+        {
+            SceneManager.LoadScene("Floor8");
+        }
+        else
+        {
+            Application.Quit();
+        }
+    }
+
+
+    
+}
