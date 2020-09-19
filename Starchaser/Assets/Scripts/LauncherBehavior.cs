077662d93aa31966fdd768d28064f6161219a2f0
Nathan <robert_roleda@dlsu.edu.ph>
2020-09-19 15:03:20 +0800

    spikes, launcher, and mushroom done

---
+++ Starchaser/Assets/Scripts/LauncherBehavior.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LauncherBehavior : MonoBehaviour
+{
+    [SerializeField] private float forwardThrust;
+    [SerializeField] private float upwardThrust;
+    private void OnTriggerEnter(Collider other)
+    {
+        UnityStandardAssets.Characters.ThirdPerson.ThirdPersonCharacter player =
+            other.GetComponent<UnityStandardAssets.Characters.ThirdPerson.ThirdPersonCharacter>();
+        if (player != null)
+        {
+            player.gameObject.GetComponent<Rigidbody>().AddForce(transform.TransformDirection(Vector3.left) * forwardThrust +
+                 transform.TransformDirection(Vector3.up) * upwardThrust);
+        }
+    }
+}
---
+++ Starchaser/Assets/Scripts/MushroomBehavior.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MushroomBehavior : MonoBehaviour
+{
+    [SerializeField] private float bouncePower;
+    private void OnTriggerEnter(Collider other)
+    {
+        UnityStandardAssets.Characters.ThirdPerson.ThirdPersonCharacter player =
+            other.GetComponent<UnityStandardAssets.Characters.ThirdPerson.ThirdPersonCharacter>();
+        if (player != null)
+        {
+            if (!player.m_IsGrounded)
+                player.GetComponent<Rigidbody>().AddForce(player.transform.TransformDirection(
+                    new Vector3(0, bouncePower, player.transform.TransformDirection(Vector3.forward).z * 25)));
+        }
+    }
+}
