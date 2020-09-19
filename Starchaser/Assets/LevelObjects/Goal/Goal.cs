b2dfd309abc9a59630a2370e6fdd187caf626126
Clymiaru <lscirno@gmail.com>
2020-09-19 15:38:27 +0800

    Goal now triggers end of level

---
+++ Starchaser/Assets/LevelObjects/Goal/Goal.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!GameManager.isLevelFinished)
+        {
+            var currentView = ViewHandler.Instance.GetActiveView();
+            currentView.Hide();
+            ViewHandler.Instance.Show(ViewNames.StarchaserScreenNames.RESULTS);
+            GameManager.LevelIsFinished();
+        }
+
+    }
+}
